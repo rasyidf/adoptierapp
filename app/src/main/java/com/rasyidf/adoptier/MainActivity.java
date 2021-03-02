fd55a079989d6f6f63470c265aff17c89a62ad27
rasyidf <ufarasfa@gmail.com>
2021-03-02 19:50:45 +0700

    Initial Commit

---
+++ app/src/main/java/com/rasyidf/adoptier/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rasyidf.adoptier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
